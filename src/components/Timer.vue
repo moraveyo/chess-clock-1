9d2f3972d141f09f278dee8519cd6155852ce633
Vladimir Musikhin <vladimir.musikhin@gmail.com>
2018-06-28 23:51:55 +0300

    Add clocks and timer

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="jumbotron jumbotron-fluid">
-    <div class="container text-center">
-      <h1 class="display-5">ХОД {{ player }}-ГО ИГРОКА</h1>
-      <p class="display-1">02:00.000</p>
-      <router-link tag="button" class="btn btn-primary" v-if="player === 1" to="2">ЗАВЕРШИТЬ ХОД</router-link>
-      <router-link tag="button" class="btn btn-primary" v-else to="1">ЗАВЕРШИТЬ ХОД</router-link>
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <h1 class="display-5">ХОД {{ player }}-ГО ИГРОКА</h1>
+          <p class="display-1">{{ normilizedTimer }}</p>
+          <router-link tag="button" class="btn btn-primary" v-if="player === 1" to="2">ЗАВЕРШИТЬ ХОД</router-link>
+          <router-link tag="button" class="btn btn-primary" v-else to="1">ЗАВЕРШИТЬ ХОД</router-link>
+        </div>
+        <div class="col-6">
+          Первый игрок - {{ firstPlayer }}
+          Второй игрок - {{ secondPlayer }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  const max = 1000 * 60 * 2;
+
   export default {
     name: "Timer",
     props: {
@@ -19,10 +29,57 @@
         default: 1
       }
     },
-    computed: {
-      timer() {
-        return
+    data() {
+      const currentTime = (new Date()).getTime();
+      return {
+        currentTime,
+        timer: max,
+        player_1: 0,
+        player_2: 0
       }
+    },
+    watch: {
+      player: function(val) {
+        this.timer = max;
+      }
+    },
+    computed: {
+      normilizedTimer() {
+        return convertTime(this.timer)
+      },
+      firstPlayer: function() {
+        return convertTime(this.player_1)
+      },
+      secondPlayer: function() {
+        return convertTime(this.player_2)
+      }
+    },
+    mounted: function() {
+      this.interval = setInterval(() => {
+        const lastTime = this.currentTime;
+        this.currentTime = (new Date()).getTime();
+
+        if ( this.timer === 0 ) return false;
+
+        const diff = this.currentTime - lastTime;
+        this[ 'player_' + this.player ] += diff;
+        this.timer = (this.timer - diff) > 0
+                   ? this.timer - diff
+                   : 0;
+      }, 1);
+    },
+    beforeDestroy: function() {
+      clearInterval(this.interval);
     }
+  }
+
+  function convertTime(unix) {
+    const t = new Date(unix);
+
+    const minutes = '0' + t.getMinutes();
+    const seconds = '0' + t.getSeconds()
+    const milliseconds = '00' + t.getMilliseconds();
+
+    return minutes.substr(-2) + ':' + seconds.substr(-2) + '.' + milliseconds.substr(-3);
   }
 </script>
--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Turn from './views/Turn'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   routes: [
     {
       path: '/:player',
